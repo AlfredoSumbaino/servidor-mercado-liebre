e1337992e7ea6400e4e75ae27aabd673c6e7ba29
Mariano Alfredo Sumbaino <alfredo-prograAndGames@outlook.com>
2025-05-25 20:43:39 -0300

    cambios de Index, arreglo del style, se agrego titulo la paginas login y register

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { createServer } from 'http'
 import { readFile } from 'fs'
-import { join, dirname } from 'path'
+import { join, dirname, extname } from 'path'
 import { getContentType } from './getContentType.js'
 import { fileURLToPath } from 'url'
 
@@ -10,8 +10,7 @@ const __dirname = dirname(__filename) // Obtener ruta de la carpeta actual
 
 // Creación del servidor HTTP
 const server = createServer((req, res) => {
-  const { method, url } = req
-
+  const { method, url } = req;
   // Manejo de solicitudes GET
   if (method === 'GET') {
     if (url === '/') {
@@ -19,22 +18,56 @@ const server = createServer((req, res) => {
       // 1. Usar readFile para leer el archivo
       // 2. Establecer el Content-Type correcto
       // 3. Enviar el contenido al cliente
+      readFile(join(__dirname, 'views', 'home.html'), 'utf8', (err, data) => {
+        if (err) {
+          res.writeHead(404, { 'Content-Type': 'text/html' })
+          res.end('<h1>404 Not Found</h1><p>El archivo solicitado no fue encontrado.</p>')
+        }else{
+          res.writeHead(200, { 'Content-Type': 'text/html' })
+          res.end(data, 'utf8')
+        }
+      })
     } else if (url === '/login') {
       // TODO: Servir el archivo login.html desde la carpeta views
       // 1. Usar readFile para leer el archivo
       // 2. Establecer el Content-Type correcto
       // 3. Enviar el contenido al cliente
+      readFile(join(__dirname, 'views', 'login.html'), 'utf8', (err, data) => {
+        if (err) {
+          res.writeHead(404, { 'Content-Type': 'text/html' })
+          res.end('<h1>404 Not Found</h1><p>El archivo solicitado no fue encontrado.</p>')
+        }else{
+          res.writeHead(200, { 'Content-Type': 'text/html' })
+          res.end(data, 'utf8')
+        }
+      })
     } else if (url === '/register') {
       // TODO: Servir el archivo register.html desde la carpeta views
       // 1. Usar readFile para leer el archivo
       // 2. Establecer el Content-Type correcto
       // 3. Enviar el contenido al cliente
+      readFile(join(__dirname, 'views', 'register.html'), 'utf8', (err, data) => {
+        if (err) {
+          res.writeHead(404, { 'Content-Type': 'text/html' })
+          res.end('<h1>404 Not Found</h1><p>El archivo solicitado no fue encontrado.</p>')
+        }else{
+          res.writeHead(200, { 'Content-Type': 'text/html' })
+          res.end(data, 'utf8')
+        }
+      })
     } else {
-      // TODO: Servir archivos estáticos desde la carpeta public (imágenes y CSS)
-      // 1. Usar join para construir la ruta del archivo
-      // 2. Usar readFile para leer el archivo
-      // 3. Establecer el Content-Type usando getContentType
-      // 4. Enviar el contenido al cliente
+      // Servir archivos estáticos desde la carpeta public (CSS, imágenes, etc.)
+      const filePath = join(__dirname, 'public', url)
+      readFile(filePath, (err, data) => {
+        if (err) {
+          res.writeHead(404, { 'Content-Type': 'text/html' })
+          res.end('<h1>404 Not Found</h1><p>El archivo solicitado no fue encontrado.</p>')
+        } else {
+          const mimeType = getContentType(filePath)
+          res.writeHead(200, { 'Content-Type': mimeType })
+          res.end(data)
+        }
+      })
     }
   }
   // Manejo de solicitudes POST
@@ -44,6 +77,8 @@ const server = createServer((req, res) => {
       // 1. Establecer el código de estado 302
       // 2. Establecer el header Location: '/'
       // 3. Finalizar la respuesta
+      res.writeHead(302, { Location: '/' })
+      res.end()
     } else {
       // Enviar respuesta 404 para rutas POST no válidas
       res.writeHead(404)
